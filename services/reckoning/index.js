fe69521c8763355261441c6897d645651cac1885
happysolomon43-boop <happysolomon43@gmail.com>
2026-09-23 09:25:59 +0100

    Export Delivery C Reckoning policy

--- services/reckoning/index.js
+++ services/reckoning/index.js
@@ -13,7 +13,7 @@ const { createShadowIntelligence } = require('./shadow');
 const { createScoringEngine } = require('./scoring');
 const { createLearningEffectsEngine } = require('./learning-effects');
 const { createReckoningStore } = require('./store');
-const { createReckoningConfig, PHASE1_RECKONING_CONFIG, DELIVERY_B_RECKONING_CONFIG } = require('./config');
+const { createReckoningConfig, PHASE1_RECKONING_CONFIG, DELIVERY_B_RECKONING_CONFIG, DELIVERY_C_RECKONING_CONFIG } = require('./config');
 const constants = require('./constants');
 const errors = require('./errors');
 const contracts = require('./contracts');
@@ -35,6 +35,7 @@ module.exports = {
   createReckoningConfig,
   PHASE1_RECKONING_CONFIG,
   DELIVERY_B_RECKONING_CONFIG,
+  DELIVERY_C_RECKONING_CONFIG,
   ...constants,
   ...errors,
   ...contracts,
